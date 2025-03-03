18975a755846c7a182d631fe23ef022442fac8c7
richa <richa@BAILEY_HOME_LAP>
2025-03-03 20:22:59 +0200

    Add project files.

---
+++ nickStudyGroup/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nickStudyGroup.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult ListPeople()
+        {
+            List<Models.PersonModel> people = new List<Models.PersonModel>();
+
+            people.Add(new Models.PersonModel { StudentNumber = "u24671691", FirstName = "Nicholas", LastName = "Bailey", Email = "u24671691@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u24661092", FirstName = "Christopher", LastName = "Bailey", Email = "u24661092@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u24464201", FirstName = "Barry", LastName = "Allen", Email = "u24464201@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u24697774", FirstName = "Cleetus", LastName = "Mcfarland", Email = "u24697774@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u24847225", FirstName = "Dwayne", LastName = "Johnson", Email = "u24847225@tuks.co.za" });
+            return View(people);
+        }
+    }
+}
